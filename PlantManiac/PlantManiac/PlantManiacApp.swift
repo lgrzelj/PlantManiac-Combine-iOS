d7af6ea26c9ad6446683cde2f9d49c2f2a385798
Lucija Grzelj <lgrzelj@foi.hr>
2025-07-21 17:57:50 +0200

    Created project.

---
+++ PlantManiac/PlantManiac/PlantManiacApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantManiacApp.swift
+//  PlantManiac
+//
+//  Created by ASELab on 21.07.2025..
+//
+
+import SwiftUI
+
+@main
+struct PlantManiacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlantManiac/PlantManiacTests/PlantManiacTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlantManiacTests.swift
+//  PlantManiacTests
+//
+//  Created by ASELab on 21.07.2025..
+//
+
+import Testing
+@testable import PlantManiac
+
+struct PlantManiacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
