095255a669c761811f8a41bb3ca525a1cb4bafa7
Lucija Grzelj <lgrzelj@foi.hr>
2025-07-25 19:31:39 +0200

    Created welcome screen which has two buttons and will use NavigationLink to have more declarative approach.

--- PlantManiac/PlantManiac/ContentView.swift
+++ PlantManiac/PlantManiac/ContentView.swift
@@ -9,14 +9,60 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world UPDATED!")
+        NavigationStack{
+            ZStack {
+                
+                Color("PrimaryBackgroundColor")
+                    .ignoresSafeArea()
+                
+                VStack (spacing: 20){
+                    Spacer()
+                    Image( "LogoIcon")
+                        .resizable()
+                        .frame(width: 100, height:100)
+                    
+                    Text("Welcome to Plant Maniac, for further app access choose an option.")
+                        .font(.custom("Itim", size: 24))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.accent)
+                        .padding(.bottom,65)
+                    
+                    //NavigationLink za deklarativni pristup
+                    NavigationLink(destination: SignIn()){
+                        
+                        Text("Sign in")
+                            .font(.custom("Itim", size: 24))
+                            .foregroundColor(.accent)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(RoundedRectangle(cornerRadius: 20)
+                                .fill(Color("ButtonColor")))
+                    }
+                    .padding(.horizontal,65)
+                    
+                    
+                    Button(action:
+                            {}){
+                        Text("Registration")
+                            .font(.custom("Itim", size: 24))
+                            .foregroundColor(.accent)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(RoundedRectangle(cornerRadius: 20)
+                                .fill(Color("ButtonColor")))
+                        
+                    }
+                            .padding(.horizontal, 65)
+                    
+                    Spacer()
+                    
+                }
+                .padding()
+                
+            }
         }
-        .padding()
-    }
+        }
+        
 }
 
 #Preview {
