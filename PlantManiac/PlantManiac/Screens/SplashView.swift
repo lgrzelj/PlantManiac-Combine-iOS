0343d9e4839d0067a90b7893cac1cd8de527ad40
Lucija Grzelj <lgrzelj@foi.hr>
2025-07-29 11:06:26 +0200

    Added SplashView in which is shown logo of app when the app is starting. Also added icons for tab bar.

---
+++ PlantManiac/PlantManiac/Screens/SplashView.swift
@@ -0,0 +1,51 @@
+//
+//  SplashView.swift
+//  PlantManiac
+//
+//  Created by ASELab on 25.07.2025..
+//
+import SwiftUI
+
+struct SplashView: View {
+    @State private var isActive = false
+    @State private var logoScale: CGFloat = 0.6
+    @State private var logoOpacity = 0.0
+
+    var body: some View {
+        if isActive {
+            ContentView()
+        } else {
+            ZStack {
+                Color("PrimaryBackgroundColor")
+                    .ignoresSafeArea()
+
+                VStack(spacing: 20) {
+                    Image("LogoIcon")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 120, height: 120)
+                        .scaleEffect(logoScale)
+                        .opacity(logoOpacity)
+
+                    Text("Plant Maniac")
+                        .font(.custom("Georgia-Italic", size: 32))
+                        .foregroundColor(.accentColor)
+                        .opacity(logoOpacity)
+                }
+            }
+            .onAppear {
+                withAnimation(.easeIn(duration: 1.2)) {
+                    logoScale = 1.0
+                    logoOpacity = 1.0
+                }
+
+                //After 2 secunds go to WelcomeScreen
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                    withAnimation {
+                        isActive = true
+                    }
+                }
+            }
+        }
+    }
+}
